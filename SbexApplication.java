8547c8fed03b01b8de91816919679333f44402dc
Ayyappa <ayyappart123@gmail.com>
2020-12-27 18:54:38 +0530

    frist commit

---
+++ SbexApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbexApplication.class, args);
+    }
+
+}
